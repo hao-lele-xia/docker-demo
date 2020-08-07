8bd528cdedd4ce23009941c8798f6bc67f9f8367
chenhao <chen.hao@unisinsight.com>
2020-08-07 17:08:39 +0800

    class for name

--- src/main/java/com/example/dockerdemo/neo4j/Neo4JController.java
+++ src/main/java/com/example/dockerdemo/neo4j/Neo4JController.java
@@ -1,12 +1,15 @@
 package com.example.dockerdemo.neo4j;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.compress.utils.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -44,6 +47,29 @@ public class Neo4JController {
             for (RelationNode relationNode : relationNodeSet){
                 log.info("id:"+parentNode.getId()+"    name:"+parentNode.getName() +"     关系："+relationNode.getName() +"子节点："+relationNode.getSonNode().getName());
             }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        List<Integer> result = new ArrayList<>(list);
+        list.forEach(t -> {
+            if(t != 2){
+                result.remove(t);
+            }
+        });
+        System.out.println(result.size());
+        list = result;
+        System.out.println(list.size());
+    }
+
+    public static void testField(){
+        Integer i = 1;
+        if(i instanceof Integer){
 
         }
     }
